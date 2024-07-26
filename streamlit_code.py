9e7deceb6bb7aeedaa3250278deeaedf5c2b7b12
AnkushSingh76 <138329430+AnkushSingh76@users.noreply.github.com>
2024-07-27 01:07:38 +0530

    Delete streamlit_code.py

--- streamlit_code.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-gradio_url = "https://9532651235ce28cfca.gradio.live/"  
-st.markdown(
-    f'<iframe src="{gradio_url}" width="100%" height="800px" style="border:none;"></iframe>',
-    unsafe_allow_html=True,
-)
